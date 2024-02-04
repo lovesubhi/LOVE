c5f759e346c542670a4bd486a10edc70e693b994
lovesubhi <154323448+lovesubhi@users.noreply.github.com>
2024-02-04 12:08:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/SubhiChiku/LOVE",
+    "https://github.com/lovesubhi/LOVE",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Suku")
 GIT_TOKEN = getenv(
