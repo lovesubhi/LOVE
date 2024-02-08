09da01dde3b686f062319c5e55bcdd188b3eb111
Akshay Gangwar <130240533+akshayxt@users.noreply.github.com>
2024-02-08 22:22:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/SubhiChiku/LOVE",
+    "https://github.com/lovesubhi/LOVE",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Suku")
 GIT_TOKEN = getenv(
