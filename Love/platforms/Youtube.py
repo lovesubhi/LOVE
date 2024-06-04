5c826b693616a44b55a307c3827f6e9835c02e61
lovesubhi <154323448+lovesubhi@users.noreply.github.com>
2024-06-04 22:32:53 +0530

    Update Youtube.py

--- Love/platforms/Youtube.py
+++ Love/platforms/Youtube.py
@@ -8,8 +8,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from LOVE.utils.database import is_on_off
-from LOVE.utils.formatters import time_to_seconds
+from Love.utils.database import is_on_off
+from Love.utils.formatters import time_to_seconds
 
 
 async def shell_cmd(cmd):
